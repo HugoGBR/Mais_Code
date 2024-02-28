9e35176034b4dd19e30c7d76f3dae8ac84bdc103
GustavoHZanella <guhzanella@gmail.com>
2024-02-27 21:33:48 -0400

    Update page.tsx
    Acrescentado detalhe de sobra. Feito delimitador de caracteres de 255 no text

--- app/Rotas/Ajustes/Produtos/page.tsx
+++ app/Rotas/Ajustes/Produtos/page.tsx
@@ -14,6 +14,15 @@ const CadastroProduto: React.FC = () => {
     const [valorProduto, setValorProduto] = useState<string>('');
     const [condicaoProduto, setCondicaoProduto] = useState<string>(''); // Removido o estado inicial
     const [descricaoProduto, setDescricaoProduto] = useState<string>('');
+    const descricaoLimiteCaracteres = 255;
+
+    const handleDescricaoChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
+        const descricao = event.target.value;
+        // Verifica se a descrição excede o limite de caracteres
+        if (descricao.length <= descricaoLimiteCaracteres) {
+            setDescricaoProduto(descricao);
+        }
+    };  
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
@@ -36,8 +45,8 @@ const CadastroProduto: React.FC = () => {
 
 
             {/* Card de Cadastro */}
-            <div className="flex justify-center items-center flex-grow">
-                <div className="max-w-lg w-full bg-white shadow-md rounded-md p-8"> {/* Removida a classe text-center do card */}
+            <div className=" flex justify-center items-center flex-grow">
+                <div className=" max-w-lg w-full bg-white shadow-xl rounded-md p-8"> {/* Removida a classe text-center do card */}
                     <h2 className="text-2xl font-semibold mb-4 text-center">Cadastro Produto</h2> {/* Centralizando apenas o título "Cadastro Produto" */}
                     <form onSubmit={handleSubmit}>
                         <div className="mb-4">
@@ -95,13 +104,16 @@ const CadastroProduto: React.FC = () => {
                                 id="descricaoProduto"
                                 name="descricaoProduto"
                                 value={descricaoProduto}
-                                onChange={(e) => setDescricaoProduto(e.target.value)}
+                                onChange={handleDescricaoChange}
                                 placeholder="Descrição do Produto"
                                 rows={4}
+                                maxLength={descricaoLimiteCaracteres}
                                 className="shadow-inner-2 mt-1 p-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                             ></textarea>
+                            {/* Exibe o contador de caracteres restantes */}
+                            <p className="text-sm text-gray-500">{descricaoProduto.length}/{descricaoLimiteCaracteres}</p>
                         </div>
-                        <div className="text-center"> {/* Centralizando o botão "CADASTRAR" */}
+                        <div className="text-center">
                             <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 CADASTRAR
                             </button>
