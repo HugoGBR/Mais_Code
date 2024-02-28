d709b2b32539b2fb26321a65905ab4a3db3d3862
JoaoReitman <ojoaoreitman@gmail.com>
2024-02-27 21:40:55 -0400

    progresso2
    progresso

--- app/Rotas/Cadastros/page.tsx
+++ app/Rotas/Cadastros/page.tsx
@@ -10,26 +10,73 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+  import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+    
 
 
 
 export default function Contrato() {
     //Faça suas const aqui(constantes)
+    
     return(
         <div className="flex items-center justify-center h-screen ">
 
-    <Card >
+    <Card>
   <CardHeader>
-    <CardTitle>Contrato</CardTitle>
+    <CardTitle>Contrato: Nº 00005</CardTitle>
     <CardDescription>Dados do Contrato</CardDescription>
   </CardHeader>
-  <input placeholder="Nome" type="text" />
-  <input placeholder="Sobrenome" type="text" />
+  <div className="grid grid-cols-2 gap-5 ">
+    <div>
+        <input placeholder="Nome" type="text" />
+    </div>
+    <div>
+        <input placeholder="Sobrenome" type="text" />
+    </div>
+    <div>
+        <input  placeholder="Data de inicio" type="text" />
+    </div>
+    <div>
+        <input placeholder="Data de termino" type="text" />
+    </div>
 
-<input placeholder="Data de inicio" type="date" />
-<input placeholder="Data de termino" type="date" />
+  </div>
 
-<h2>Modelo de Contrato</h2>
+
+<h2>Modelo de Contrato:</h2>
+<div className="gap-5">
+    <div>
+<Select>
+<SelectTrigger className="w-[180px] gap-3">
+    <SelectValue placeholder="Continuidade" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="teste">Pontual</SelectItem>
+    <SelectItem value="testee">Contínuo</SelectItem>
+  </SelectContent>
+</Select>
+    </div>
+<div>
+<Select>
+<SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Hora Dev" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="hrdev1">PHP</SelectItem>
+    <SelectItem value="hrdev2">Python</SelectItem>
+    <SelectItem value="hrdev3">React</SelectItem>
+  </SelectContent>
+</Select>
+</div>
+
+</div>
+
 
 </Card>
         </div>
