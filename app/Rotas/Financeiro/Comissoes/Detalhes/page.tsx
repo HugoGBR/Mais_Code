fd2432d7757be2c03e1ce8687df1c1355de46e53
jeffalve <jam991271@gmail.com>
2024-02-28 22:45:16 -0300

    Desenvolvimento-detalhes-comissao
    Comecei o desenvolvimento da tela de detalhes de comissoes

--- app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
+++ app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
@@ -1,8 +1,29 @@
 'use client'
+import { Grid } from "lucide-react"
 import React from "react"
 
 export default function Detalhescomissao(){
     return(
-        <div>detalhes de comissao</div>
+        <div className="grid grid-cols-2">
+            <h1>
+                Detalhes contrato
+            </h1> 
+                
+            <div>
+                teste
+            </div>
+            <div className="grid grid-cols-3">
+                <div>
+                    <h1>01</h1>
+                    <input className="border-b-4 " type="text" />
+                </div>
+                <div>02</div>
+                <div>03</div>
+                <div>04</div>
+                <div>05</div>
+                <div>06</div>
+            </div>
+            
+        </div>
         )
 }
