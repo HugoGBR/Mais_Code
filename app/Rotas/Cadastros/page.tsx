8b4343af6c3ced7d5f59d22aecc261c6560d2345
JoaoReitman <ojoaoreitman@gmail.com>
2024-02-29 21:47:10 -0400

    ta quase

--- app/Rotas/Cadastros/page.tsx
+++ app/Rotas/Cadastros/page.tsx
@@ -1,93 +1,134 @@
 'use cliente'
+import { useState } from "react"
 import { Card } from "@/components/ui/card"
 import React from "react"
+import { Checkbox } from "@/components/ui/checkbox"
+
 import {
-    
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-  import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-  
+
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 export default function Contrato() {
-    //Faça suas const aqui(constantes)
-    
-    return(
-        <div className="flex items-center justify-center h-screen ">
+  //Faça suas const aqui(constantes)
 
-    <Card className="w-1/3">
-  <CardHeader>
-    <CardTitle>Contrato: Nº 00005</CardTitle>
-    <CardDescription>Dados do Contrato</CardDescription>
-  </CardHeader>
-  <div className="grid grid-cols-2 gap-5 ">
-    <div>
-        <input placeholder="Nome" type="text" />
+  return (
+    <div className="grid w-2/3 grid-cols-2">
+      <div className="">
+        <Card className="w-auto p-10">
+          <form>
+            <div className="flex justify-between mb-6 text-2xl font-bold">
+              <h1>Contrato</h1>
+              <h1>Nº 00005</h1>
+            </div>
+
+            <h1 className="mb-4">Dados do Contrato</h1>
+            <div className="grid grid-cols-2 gap-5 mb-4">
+              <input className="border-b-2" placeholder="Nome" type="text" />
+              <input className="border-b-2" placeholder="Sobrenome" type="text" />
+              <input className="border-b-2" placeholder="Data de inicio" type="text" />
+              <input className="border-b-2" placeholder="Data de termino" type="text" />
+            </div>
+            <label className="" htmlFor="DadosContrato">Modelo do Contrato</label>
+            <div className="grid grid-cols-2 grid-rows-2 mt-5">
+              <Select>
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Continuidade" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="teste">Pontual</SelectItem>
+                  <SelectItem value="testee">Contínuo</SelectItem>
+                </SelectContent>
+              </Select>
+              <div className="flex flex-col ml-5">
+                <label htmlFor="teste">Horas Trabalhadas</label>
+                <input className="w-10 border-b-2" placeholder="0000" type="text" />
+              </div>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Hora Dev" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="teste">Python</SelectItem>
+                  <SelectItem value="testee">php</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <h2>Dados do Contato</h2>
+            <div className="grid grid-cols-2">
+
+              <input className="border-b-2" placeholder="Nome:" type="text" />
+              <input className="border-b-2" placeholder="Contato" type="text" />
+              <input className="border-b-2" placeholder="Email:" type="text" />
+            </div>
+
+          </form>
+        </Card>
+      </div>
+
+
+
+
+
+
+
+
+      <div className="w-auto">
+
+        <Card className="w-auto p-10">
+          <form>
+            <div className="flex justify-between mb-6 text-2xl font-bold">
+              <h1>Forma de Pagamento</h1>
+
+            </div>
+            <div className="flex flex-col mb-4 ml-5">
+              <label htmlFor="teste">Valor da Entrada</label>
+              <input className="border-b-2 " placeholder="R$ 0000,00" type="text" />
+            </div>
+            
+            <div className="">
+              <label htmlFor="Nn">Status Cliente</label>
+            </div>
+
+            <div className="grid grid-cols-2 grid-rows-2 mt-5">
+              <Select>
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Novo" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="teste">Novo</SelectItem>
+                  <SelectItem value="testee">Antigo</SelectItem>
+                </SelectContent>
+              </Select>
+
+
+
+            </div>
+            <h2>Dados do Contato</h2>
+            <div className="grid grid-cols-2">
+
+              <input className="border-b-2" placeholder="Nome:" type="text" />
+              <input className="border-b-2" placeholder="Contato" type="text" />
+              <input className="border-b-2" placeholder="Email:" type="text" />
+            </div>
+
+          </form>
+        </Card>
+
+      </div>
     </div>
-    <div>
-        <input placeholder="Sobrenome" type="text" />
-    </div>
-    <div>
-        <input  placeholder="Data de inicio" type="text" />
-    </div>
-    <div>
-        <input placeholder="Data de termino" type="text" />
-    </div>
 
-  </div>
-
-
-<div className="grid grid-cols-2 gap-5 mt-5 ">
-<label className="" htmlFor="DadosContrato">Dados do Contrato</label>
-    <div>
-<Select>
-<SelectTrigger className="w-[180px] gap-3">
-    <SelectValue placeholder="Continuidade" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="teste">Pontual</SelectItem>
-    <SelectItem value="testee">Contínuo</SelectItem>
-  </SelectContent>
-</Select>
-    </div>
-<div className="grid grid-cols-2 gap-5 ">
-<Select>
-<SelectTrigger className="w-[180px]">
-    <SelectValue placeholder="Hora Dev" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="hrdev1">PHP</SelectItem>
-    <SelectItem value="hrdev2">Python</SelectItem>
-    <SelectItem value="hrdev3">React</SelectItem>
-  </SelectContent>
-</Select>
-<div>
-  <label htmlFor="teste">Horas Trabalhadas</label> <br />
-  <input placeholder="0000" type="text" />
-</div>
-<div>
-  <h2>Dados do Contato</h2>
-
-  <input placeholder="Nome:" type="text" /> 
-  <input placeholder="Contato" type="text" />
-  <input placeholder="Email:" type="text" />
-</div>
-</div>
-
-</div>
-
-
-</Card>
-        </div>
-        
-    )
+  )
 }
