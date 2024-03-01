8c063604a641c0745998ca264a73da2c8ecc870c
Horto - Lab. 303 <horlab303@senacmsedu.local>
2024-02-29 21:38:43 -0400

    Fiz alteração no popup para fechar ao clicar em cancelar, e quase finalizado a validação. Falta fazer a máscara no cpf.

--- app/Rotas/Perfil/page.tsx
+++ app/Rotas/Perfil/page.tsx
@@ -23,10 +23,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { useState } from 'react'
+
+//pequisar rejex
+
 
 const formulario = z.object({
+  //Números de restrição apenas para teste
   nome: z.string().min(10, 'Digite no mínimo 10 caracteres.').max(60, 'Digite o nome completo, máximo de 60 caracteres.'),
-  cpf: z.string().min(14, 'O CPF deve conter 14 caracteres.'),
+  cpf: z.string().min(11, 'O CPF deve conter 11 caracteres.').max(11,'O CPF deve conter 11 caracteres.'),
   email: z.string().email("Email inválido."),
   senha: z.string().min(5, 'Número mínimo de caracteres é 5.').max(10, "Atingiu o limite de caracteres.")
 })
@@ -35,21 +40,23 @@ type formulario = z.infer<typeof formulario>
 
 
 
-export default function Home() {
+export default function Perfil() {
   const { register, handleSubmit, formState: { errors } } = useForm<formulario>({
     resolver: zodResolver(formulario)
   })
-
-  function handleForm(dados: formulario) {
-    route.push('https://en.wikipedia.org/wiki/Monkey')
+  const [isOpen, setIsOpen] = useState(false)
+function handleForm(dados: formulario) {
+  setIsOpen(true)
+  // route.push('')
     console.log(dados)
   }
-
+  
+  
   const route = useRouter();
 
   return (
 
-      <div  className='flex justify-center items-center h-screen bg-gray-100'>
+      <div className='flex justify-center items-center h-screen bg-gray-100'>
         <Card className='p-6 drop-shadow-xl rounded-xl'>
             <form onSubmit={handleSubmit(handleForm)}>
             <div>
@@ -74,35 +81,36 @@ export default function Home() {
 
             <div className='pb-16 grid grid-cols-1 sm:grid-cols-2 gap-4'>
               <div className=''>
-                <input type="text" placeholder='João da Silva'{...register('nome')} 
+                <input type="text" placeholder='João da Silva' {...register('nome')} 
                 className='focus:outline-none border border-t-0 border-l-0 border-r-0 border-b-gray-600 p-2 mt-1'/>
-                {errors.nome && (<div className='text-red-500 font-semibold'>{errors.nome.message}</div>)}
+                {errors.nome && (<div className='text-red-500'>{errors.nome.message}</div>)}
               </div>
 
               <div>
                 <input type='text' placeholder='999.999.999-99'{...register('cpf')} 
                 className='focus:outline-none border border-t-0 border-l-0 border-r-0 border-b-gray-600  p-2 mt-1' />
-                {errors.cpf && (<div className='text-red-500 font-semibold'>{errors.cpf.message}</div>)}
+                {errors.cpf && (<div className='text-red-500'>{errors.cpf.message}</div>)}
               </div>
 
               <div>
                 <input type='email' placeholder='joão@gmail.com' {...register('email')} 
                 className='focus:outline-none border border-t-0 border-l-0 border-r-0 border-b-gray-600  p-2 mt-1' />
-                {errors.email && (<div className='text-red-500 font-semibold'>{errors.email.message}</div>)}
+                {errors.email && (<div className='text-red-500'>{errors.email.message}</div>)}
               </div>
 
               <div>
                 <input type='password' placeholder='************' {...register('senha')} 
                 className='focus:outline-none border border-t-0 border-l-0 border-r-0 border-b-gray-600  p-2 mt-1'  />
-                {errors.senha && (<div className='text-red-500 font-semibold'>{errors.senha.message}</div>)}
+                {errors.senha && (<div className='text-red-500 '>{errors.senha.message}</div>)}
               </div>
             </div>
-
+            <div className='flex justify-center'>
+                <Button variant="outline"className="transition duration-300 ease-in-out bg-blue-800 hover:bg-blue-400 text-white rounded-xl  md:w-56 shadow-xl ">Editar</Button>
+            </div>
             </form>
             <CardFooter className="flex justify-center items-center">
-              <Dialog>
+              <Dialog open = {isOpen}>
                 <DialogTrigger asChild>
-                  <Button variant="outline"className=" transition duration-300 ease-in-out bg-blue-800 hover:bg-blue-400 text-white rounded-xl  md:w-56 shadow-xl ">Editar</Button>
                 </DialogTrigger>
                 <DialogContent className="w-auto rounded-lg">
                   <DialogHeader>
@@ -111,7 +119,7 @@ export default function Home() {
                   <DialogFooter className="flex justify-center items-center">
                     <div className="space-x-7">
                       <Button className="w-32 border  border-green-500 text-black font-semibold bg-white transition duration-500 ease-in-out hover:bg-green-500 hover:text-white" type="button">Confirmar </Button>
-                      <Button className="w-32 border border-red-500 text-black font-semibold bg-white transition duration-500 ease-in-out hover:bg-red-500 hover:text-white" type="button"> Cancelar</Button>
+                      <Button className="w-32 border border-red-500 text-black font-semibold bg-white transition duration-500 ease-in-out hover:bg-red-500 hover:text-white" onClick={()=>setIsOpen(false)}  type="button"> Cancelar</Button>
                     </div>
                   </DialogFooter>
                 </DialogContent>
