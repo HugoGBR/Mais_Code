4156d1258fa5c0c67dce1c82ead9cedaa3762e64
HugoGBR <hugopn12@gmail.com>
2024-02-29 23:37:44 -0400

    Arrumando popup
    Arrumando importe perfil

--- app/Rotas/Perfil/page.tsx
+++ app/Rotas/Perfil/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import PopupConfirmacao from "@/components/PopUpConfirm"
+import PopupConfirmacao from '@/components/PopUpConfirmacao'
 import { z } from 'zod'
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button"
