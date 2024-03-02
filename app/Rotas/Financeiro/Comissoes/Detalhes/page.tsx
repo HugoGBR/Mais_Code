702b64e19fc96cf79c3ff8e127802ceb2ed62e9c
Victor Vicente <victorvpenteadog@gmail.com>
2024-03-01 21:29:09 -0400

    Nao consegui ajustar para deixar no centro so isso que falta

--- app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
+++ app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
@@ -1,8 +1,23 @@
 'use client'
-import { Grid } from "lucide-react"
-
-
-
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import * as React from "react"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import {
     Card,
     CardContent,
@@ -50,8 +65,95 @@ export default function Detalhescomissao() {
                     </form>
                 </CardContent>
             </Card>
-        </div>
+            <Card className="flex justify-center h-[700px] w-[500px] shadow-xl ">
+                <CardHeader className="flex flex-col">
+                    <CardTitle>Detalhes parcelas</CardTitle>
+                </CardHeader>
+                <ScrollArea className="h-[600px] w-[400px] rounded-md border p-4">
+                    <Table>
+                        <TableHeader>
+                            <TableRow className="grid-cols-3 grid-rows-3 space-x-9">
+                                <TableHead className="text-lg text-black ">Dias</TableHead>
+                                <TableHead className="text-lg text-center text-black">Parcelas</TableHead>
+                                <TableHead className="text-lg pl-7 text-black">Comissão</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="text-center">1x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">2x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="text-center">3x</TableCell>
+                                <TableCell className="text-center">$250.00</TableCell>
+                            </TableRow>
+                        </TableBody>
 
+                    </Table>
+                </ScrollArea>
+
+            </Card>
+        </div>
     )
 }
 
