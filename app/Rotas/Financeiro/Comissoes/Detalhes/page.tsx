f769e15af6f7cf22b7034ba28f3df864a3e000ad
Victor Vicente <victorvpenteadog@gmail.com>
2024-03-05 19:29:39 -0400

    Alteracao e centralizei coisas

--- app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
+++ app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
@@ -65,92 +65,156 @@ export default function Detalhescomissao() {
                     </form>
                 </CardContent>
             </Card>
-            <Card className="flex justify-center h-[700px] w-[500px] shadow-xl ">
-                <CardHeader className="flex flex-col">
+            <Card className=" h-[700px] w-[600] shadow-xl">
+                <CardHeader className="items-center">
                     <CardTitle>Detalhes parcelas</CardTitle>
                 </CardHeader>
-                <ScrollArea className="h-[600px] w-[400px] rounded-md border p-4">
-                    <Table>
-                        <TableHeader>
-                            <TableRow className="grid-cols-3 grid-rows-3 space-x-9">
-                                <TableHead className="text-lg text-black ">Dias</TableHead>
-                                <TableHead className="text-lg text-center text-black">Parcelas</TableHead>
-                                <TableHead className="text-lg pl-7 text-black">Comissão</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell className="text-center">1x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">2x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="text-center">3x</TableCell>
-                                <TableCell className="text-center">$250.00</TableCell>
-                            </TableRow>
-                        </TableBody>
+                <div className="flex space-x-2">
+                    <ScrollArea className="h-[600px] w-[400px] rounded-md border p-4">
+                        <Table>
+                            <TableHeader>
+                                <TableRow className="grid-cols-3 grid-rows-3 space-x-5">
+                                    <TableHead className="text-lg text-black text-center">Dias</TableHead>
+                                    <TableHead className="text-lg text-center text-black">Parcelas</TableHead>
+                                    <TableHead className="text-lg pl-7 text-black">Comissão</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                            </TableBody>
 
-                    </Table>
-                </ScrollArea>
+                        </Table>
+                    </ScrollArea>
+                    <ScrollArea className="h-[600px] w-[400px] rounded-md border p-4">
+                        <Table>
+                     
+
+                            <TableBody>
+                            <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell className="text-center">20/01/2023</TableCell>
+                                    <TableCell className="text-center">1x R$250.00</TableCell>
+                                    <TableCell className="text-center">R$104.00</TableCell>
+                                </TableRow>
+                            </TableBody>
+
+                        </Table>
+                    </ScrollArea>
+                </div>
+
 
             </Card>
         </div>
