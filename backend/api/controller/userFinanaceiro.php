4709a72a1d769d3b85b1a79ad2edbd079bdc3722
stopassolli01 <139408201+stopassolli01@users.noreply.github.com>
2024-05-13 21:10:12 -0400

    eu fiz  controlerFinanceiro

---
+++ backend/api/controller/userFinanaceiro.php
@@ -0,0 +1,22 @@
+<?php
+include "../database.php"; 
+
+class Usercontroller
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $objDb = new Database;
+        $this->conn = $objDb->connect();
+    }
+
+    public function getAllUsers()
+    {
+        $sql = "SELECT * FROM usuarios";
+        $db = $this->conn->prepare($sql);
+        $db->execute();
+        $users = $db->fetchAll(PDO::FETCH_ASSOC);
+        return $users;
+    }
+}
