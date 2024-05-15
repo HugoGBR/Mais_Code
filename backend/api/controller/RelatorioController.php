71a47ce1d0b1dfd74288942cc66b16ae3452f49e
gutosil <143763083+gutosil@users.noreply.github.com>
2024-05-14 21:11:51 -0400

    Create RelatorioController.php

---
+++ backend/api/controller/RelatorioController.php
@@ -0,0 +1,23 @@
+<?php
+include "../database.php"; //importando database
+
+class Usercontroller
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $objDb = new Database;
+        $this->conn = $objDb->connect();
+    }
+
+    public function getAllUsers()
+    {
+        $sql = "SELECT Cliente.id ,cliente_id,Cliente.nome,Vendedor.id,Vendedor.nome FROM Contrato JOIN Cliente ON Cliente.id = Contrato.cliente_id JOIN Vendedor ON Vendedor.id = Contrato.vendedor_id;";
+        $db = $this->conn->prepare($sql);
+        $db->execute();
+        $users = $db->fetchAll(PDO::FETCH_ASSOC);
+        return $users;
+    }
+
+}    
