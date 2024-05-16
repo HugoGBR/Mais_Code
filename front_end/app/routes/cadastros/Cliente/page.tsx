09878c6f1be4a74695a09b0664deb0698ed0ad1c
DaniloMamedio <danilomamedio95000@gmail.com>
2024-05-16 20:59:37 -0300

    Alteração na Pag. Cadastro de Contrato, e Gestão Usuários
    Na pag Cadastro de Contrato ajustei as abas Contrato e Cliente que estavam selecionadas sem estar nela. Na pag Gestão Usuários alterei o dropdown e alinhei com o layout dos inputs

--- front_end/app/routes/cadastros/Cliente/page.tsx
+++ front_end/app/routes/cadastros/Cliente/page.tsx
@@ -12,11 +12,11 @@ export default function Contrato() {
             <div className="flex gap-5">
                 <div>
                     <Link href="/routes/cadastros"
-                          className="font-bold text-blue-700 border-b-2 border-blue-700">Contrato</Link>
+                          className= "focus:font-bold focus:text-blue-700 focus:border-b-2 focus:outline-none focus:border-blue-500">Contrato</Link>
                 </div>
                 <div>
                     <Link href="/routes/cadastros/Cliente"
-                          className="focus:font-bold focus:text-blue-700 focus:border-b-2 focus:outline-none focus:border-blue-500">Cliente</Link>
+                          className="focus:font-bold text-blue-700 border-b-2  border-blue-700">Cliente</Link>
                 </div>
             </div>
             <div>
