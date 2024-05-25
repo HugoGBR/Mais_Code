a57f961a81ac225c39ab3266cc13c1b3d410d637
JoaoReitman <139408527+JoaoReitman@users.noreply.github.com>
2024-05-24 21:40:43 -0400

    FInalizei VendaController do front
    Finalizado amigos

--- front_end/lib/VendaController.ts
+++ front_end/lib/VendaController.ts
@@ -21,15 +21,15 @@ export async function getAllProductById(){
 }
 
 // Faz o insert
-// export async function createNewSell(tipo_contrato_id: number, parcela_id: number, inicio_contrato: Date, final_contrato: Date, valor_entrada: number, valor_total: number, nome_contato: string, email: string, telefone: number, metodo_pagamento: number) {
-//     const request = await fetch("http://localhost/Mais_code/backend/api/service/venda.php?acao=createNewSell",
-//         {
-//             method: "POST",
-//             body: JSON.stringify({ contrato_id: tipo_contrato_id, email: newEmail, telefone: newTelefone,cpf_cnpj: newCpf_cnpj  })
-//         });
+export async function createNewSell(tipo_contrato_id: number, parcela_id: number, inicio_contrato: Date, final_contrato: Date, valor_entrada: number, valor_total: number, nome_contato: string, email: string, telefone: number, metodo_pagamento: number) {
+     const request = await fetch("http://localhost/Mais_code/backend/api/service/venda.php?acao=createNewSell",
+         {
+            method: "POST",
+             body: JSON.stringify({ contrato_id: tipo_contrato_id, parcela: parcela_id, iniciocontrato: inicio_contrato,finalcontrato: final_contrato,valor_de_entrada: valor_entrada, valordototal: valor_total, nomedocontato: nome_contato, newemail: email,newtelefone: telefone, new_metodo_pagamento: metodo_pagamento})
+         });
 
-//     const response = await request.json();
-//     console.log(response)
+    const response = await request.json();
+     console.log(response)
 
-//     return response
-// }
+    return response
+ }
