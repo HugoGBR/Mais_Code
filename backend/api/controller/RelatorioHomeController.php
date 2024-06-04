b3eab5d73cbd0b26b0dfd9216b5e89104c6b72b2
DaniloMamedio <danilomamedio95000@gmail.com>
2024-06-03 23:06:46 -0300

    Criação do backend da home
    Ainda estou no processo de desenvolvimento, mas para não perder oq já fiz vou dar commit

---
+++ backend/api/controller/RelatorioHomeController.php
@@ -0,0 +1,34 @@
+<?php
+include "../database.php"; //importando database
+
+class RelatorioHomeController
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $objDb = new Database;
+        $this->conn = $objDb->connect();
+    }
+
+    public function getRelatorioHome()
+    {
+        $sql = "SELECT 
+        vendas.id as NºContrato,
+        inicio_contrato as Data_Venda,
+        clientes.nome as Cliente,
+        vendas.valor_total as Valor,
+        vendas.status
+    FROM 
+        vendas
+    join 
+        clientes on vendas.cliente_id = clientes.id
+    order by 
+        vendas.inicio_contrato desc
+    limit 10;";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $relatorio_home = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $relatorio_home;
+    }
+}
