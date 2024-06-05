2ba8833b25079b4a2231b42bd4873a0c32eff57a
stopassolli01 <139408201+stopassolli01@users.noreply.github.com>
2024-06-04 21:15:50 -0400

    coloquei 8 cards falta a paginaçao

--- front_end/app/routes/financeiro/page.tsx
+++ front_end/app/routes/financeiro/page.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import CardUsuario from '@/components/CardUsuario';
-import {dadosUsuario} from "@/lib/interfaces/dadosUsuarios";
+import { dadosUsuario } from "@/lib/interfaces/dadosUsuarios";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
-import {getAllFinan} from '@/lib/Financeirocontroler';
-
-
+import { useRouter } from "next/navigation";
+import { getAllFinan } from '@/lib/Financeirocontroler';
 
 
 export default function Gestao() {
     const [listaUsuarios, setListaUsuarios] = useState<dadosUsuario[]>([]);
     const [carregando, setCarregando] = useState(true);
     const router = useRouter();
-
+ b
     const rotaNewUser = () => {
         router.push('/routes/gestao/Usuario');
     }
@@ -28,21 +26,23 @@ export default function Gestao() {
     useEffect(() => {
         carregarUfinan();
     }, []);
-
+    
+    
 
     return (
-        
-        <div className="flex md:grid md:grid-cols-2 space-x-4">
-            {listaUsuarios.map(item => (
-                <Link href={`/routes/gestao/users/${item.id}`} key={item.id}>
-                    <div key={item.id} className='bg-gray-300 mb-4 rounded-lg' style={{width: '100%'}}>
-                        <a className="block">
-                            <CardUsuario dados={item}/>
-                        </a>
-                    </div>
-                </Link>
-            ))}
+        <div>
+            <div className="flex md:grid md:grid-cols-2 space-x-4">
+                {listaUsuarios.slice(0, 8).map(item => (
+                    <Link href={`/routes/gestao/users/${item.id}`} key={item.id}>
+                        <div key={item.id} className='bg-gray-300 mb-4 rounded-lg' style={{ width: '100%' }}>
+                            <a className="block">
+                                <CardUsuario dados={item} />
+                            </a>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+           
         </div>
     )
 }
-
