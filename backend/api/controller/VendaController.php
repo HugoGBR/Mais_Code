c05eb2fa5a70bf43834977a2a21ab3cbf2e6b5a7
JoaoReitman <139408527+JoaoReitman@users.noreply.github.com>
2024-06-11 21:37:37 -0400

    solucao de problemas

--- backend/api/controller/VendaController.php
+++ backend/api/controller/VendaController.php
@@ -31,23 +31,34 @@ class Vendacontroller
 
     public function CreateNewSell()
     {
-        $user = json_decode(file_get_contents("php://input"));
-        $sql = "INSERT INTO vendas(inicio_contrato,final_contrato,nome_contato,telefone,email,metodo_pagamento)
-        VALUES (:inicio_contrato,:final_contrato,:nome_contato,:telefone,:email,:metodo_pagamento)";
-        $db = $this->conn->prepare($sql);
-        $db->bindParam(":inicio_contrato", $user->inicio_contrato);
-        $db->bindParam(":final_contrato", $user->final_contrato);
-        $db->bindParam(":nome_contato", $user->nome_contato);
-        $db->bindParam(":telefone", $user->telefone);
-        $db->bindParam(":email", $user->email);
-        $db->bindParam(":metodo_pagamento", $user->metodo_pagamento);
-        $db->execute();
 
-        if ($db->execute()) {
-            $resposta = ["Mensagem" => "Venda Cadastrada com Sucesso!"];
+        try {
+            $user = json_decode(file_get_contents("php://input"));
+            $sql = "INSERT INTO vendas(cliente_id,tipo_contrato_id,parcela_id,produto_id,usuario_id,inicio_contrato,final_contrato,nome_contato,telefone,email,metodo_pagamento)
+            VALUES (:cliente_id,:tipo_contrato_id,:parcela_id,:produto_id,:usuario_id,:inicio_contrato,:final_contrato,:nome_contato,:telefone,:email,:metodo_pagamento)";
+            $db = $this->conn->prepare($sql);
+            $db->bindParam(":cliente_id", $user->cliente_id);
+            $db->bindParam(":tipo_contrato_id", $user->tipo_contrato_id);
+            $db->bindParam(":parcela_id", $user->parcela_id);
+            $db->bindParam(":produto_id", $user->produto_id);
+            $db->bindParam(":usuario_id", $user->usuario_id);
+
+            $db->bindParam(":final_contrato", $user->final_contrato);
+            $db->bindParam(":nome_contato", $user->nome_contato);
+            $db->bindParam(":telefone", $user->telefone);
+            $db->bindParam(":email", $user->email);
+            $db->bindParam(":metodo_pagamento", $user->metodo_pagamento);
+            $db->execute();
+
+            if ($db->execute()) {
+                $resposta = ["Mensagem" => "Venda Cadastrada com Sucesso!"];
+            }
+
+            return $resposta;
+        } catch (\Exception $e) {
+            error_log('Erro ao criar usuário: ' . $e->getMessage());
+            return json_encode(['status' => 0, 'message' => 'Erro ao criar usuário.']);
         }
-
-        return $resposta;
     }
 
     // public function CreateNewSell()
