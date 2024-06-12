bfaf08f396ecddc75579d0832c62efa3edec1c21
DaniloMamedio <danilomamedio95000@gmail.com>
2024-06-12 18:36:59 -0400

    Pódio Vendedores Home
    Pódio finalizado, criei um componente chamado PodioVendedor e chamei na page da home. Testei e está funcionando corretamente

--- front_end/app/routes/home/page.tsx
+++ front_end/app/routes/home/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CadastroLink from './CompCadastro';
 import RelatorioHome from '@/components/relatorioHome';
+import TopVendedores from '@/components/PodioVendedor';
 
 const HomePage = () => {
     return (
@@ -16,15 +17,7 @@ const HomePage = () => {
 
             <div className='h-auto gap-2 w-full flex flex-col lg:flex lg:flex-row'>
                 <div className='h-auto w-full md:w-2/5 bg-white drop-shadow-xl grid grid-cols-1 items-center rounded-xl'>
-                    <div className=''>
-                        <p className='underline text-lg lg:text-6xl ml-20 font-bold text-yellow-500'>#1 Gustavo</p>
-                    </div>
-                    <div className=''>
-                        <h2 className='underline ml-20 text-lg lg:text-5xl font-bold text-gray-400'>#2 João</h2>
-                    </div>
-                    <div className=''>
-                        <h3 className='underline ml-20 text-lg lg:text-4xl font-bold text-amber-700'>#3 Hugo</h3>
-                    </div>
+                    <TopVendedores/>
                 </div>
                                                 
                 <div className='bg-white drop-shadow-xl text-center w-full overflow-y-auto md:w-3/5 pt-5 h-auto rounded-xl'>
