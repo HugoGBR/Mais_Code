58a906d0dfb9e76c26fd58507b602f7e4cea2ccb
DaniloMamedio <danilomamedio95000@gmail.com>
2024-06-11 20:49:13 -0400

    Relatório da Home
    Relatório da home finalizado

--- backend/api/controller/RelatorioHomeController.php
+++ backend/api/controller/RelatorioHomeController.php
@@ -31,4 +31,26 @@ class RelatorioHomeController
         $relatorio_home = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $relatorio_home;
     }
+
+
+    public function getPodiumVendedor()
+    {
+        $sql = "SELECT 
+        vendas.id as NºContrato,
+        inicio_contrato as Data_Venda,
+        clientes.nome as Cliente,
+        vendas.valor_total as Valor,
+        vendas.status
+    FROM 
+        vendas
+    join 
+        clientes on vendas.cliente_id = clientes.id
+    order by 
+        vendas.inicio_contrato desc
+    limit 10;";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $relatorio_home = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $relatorio_home;
+    }
 }
--- front_end/app/routes/home/page.tsx
+++ front_end/app/routes/home/page.tsx
@@ -1,13 +1,4 @@
 import React from 'react';
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
 import CadastroLink from './CompCadastro';
 import RelatorioHome from '@/components/relatorioHome';
 
@@ -16,7 +7,7 @@ const HomePage = () => {
         <div className="bg-gray-100  w-11/12 -mt-5">
             <div className='h-48 w-auto -mb-3'>
                 <h1 className='text-2xl font-semibold min-[320px]:hidden max-[600px]:hidden lg:inline-block'>Bem Vindo, Calebe</h1>
-                <p className='min-[320px]:hidden max-[600px]:hidden lg:block'>ficamos felizes em vê-lo novamente</p>
+                <p className='min-[320px]:hidden max-[600px]:hidden lg:block'>Ficamos felizes em vê-lo novamente</p>
                 <div className='rounded-xl float-right bg-white drop-shadow-xl h-24 justify-center items-center'>
                     <h3 className='mt-3 ml-10 mr-10 text-lg'>Remuneraçao</h3>
                     <h1 className='text-4xl font-bold ml-10 mr-10 '>R$5000,00</h1>
@@ -41,48 +32,7 @@ const HomePage = () => {
                         <div className='max-h-72'>
                         <RelatorioHome/> 
                         </div>                    
-                      {/* <Table className='w-full'>
-                        <TableCaption></TableCaption>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-1/4">N. Contrato</TableHead>
-                                <TableHead className="w-1/4">Data</TableHead>
-                                <TableHead className="w-1/4">Cliente</TableHead>
-                                <TableHead className="w-1/4">Valor</TableHead>
-                                <TableHead className="text-right w-1/4">Status</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody className=''>
-                            <TableRow className=''>
-                                <TableCell className="font-medium bg-gray-100 rounded-l-lg border-l-4 border-l-green-800">#1201</TableCell>
-                                <TableCell className="font-medium bg-gray-100">13/08/2023</TableCell>
-                                <TableCell className="font-medium bg-gray-100">Senac</TableCell>
-                                <TableCell className="font-medium bg-gray-100">R$1000,00</TableCell>
-                                <TableCell className="text-right font-medium bg-gray-100 rounded-r-lg">Ativo</TableCell>
-                            </TableRow>
-                            <TableRow className=''>
-                                <TableCell className="font-medium bg-gray-100 rounded-l-lg border-l-4 border-l-green-800">#1201</TableCell>
-                                <TableCell className="font-medium bg-gray-100">13/08/2023</TableCell>
-                                <TableCell className="font-medium bg-gray-100">Senac</TableCell>
-                                <TableCell className="font-medium bg-gray-100">R$1000,00</TableCell>
-                                <TableCell className="text-right font-medium bg-gray-100 rounded-r-lg">Ativo</TableCell>
-                            </TableRow>
-                            <TableRow className=''>
-                                <TableCell className="font-medium bg-gray-100 rounded-l-lg border-l-4 border-l-green-800">#1201</TableCell>
-                                <TableCell className="font-medium bg-gray-100">13/08/2023</TableCell>
-                                <TableCell className="font-medium bg-gray-100">Senac</TableCell>
-                                <TableCell className="font-medium bg-gray-100">R$1000,00</TableCell>
-                                <TableCell className="text-right font-medium bg-gray-100 rounded-r-lg">Ativo</TableCell>
-                            </TableRow>
-                            <TableRow className=''>
-                                <TableCell className="font-medium bg-gray-100 rounded-l-lg border-l-4 border-l-green-800">#1201</TableCell>
-                                <TableCell className="font-medium bg-gray-100">13/08/2023</TableCell>
-                                <TableCell className="font-medium bg-gray-100">Senac</TableCell>
-                                <TableCell className="font-medium bg-gray-100">R$1000,00</TableCell>
-                                <TableCell className="text-right font-medium bg-gray-100 rounded-r-lg">Ativo</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>  */}
+                     
                 </div>
             </div>
 
