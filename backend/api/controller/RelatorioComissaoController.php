ebf24dea9634f4ffedd893975a46fa23cd1eb41d
Katypires <katianepiresrc@gmail.com>
2024-06-25 21:23:06 -0400

    Atualizado

--- backend/api/controller/RelatorioComissaoController.php
+++ backend/api/controller/RelatorioComissaoController.php
@@ -14,34 +14,40 @@ class RelatorioComissaoController
 
     public function gerarRelatorioComissao(int $id)
     {
-        $sql = "SELECT
-        vendas.tipo_contrato_id,
-        vendas.inicio_contrato,
-        clientes.nome AS nome_cliente,
-        tipo_contrato.nome AS nome_tipo_contrato,
-        parcelas.numero_parcela,
-        vendas.valor_total
-    FROM
-        vendas
-    JOIN
-    clientes ON vendas.cliente_id = clientes.id
-    JOIN
-    tipo_contrato ON vendas.tipo_contrato_id = tipo_contrato.id
-    JOIN
-    parcelas ON vendas.parcela_id = parcelas.id
-    WHERE
-        vendas.status IN ('concluido', 'em andamento')  
-    ORDER BY
-        vendas.inicio_contrato DESC
-    LIMIT 0, 1000;";
+            $sql = "SELECT 
+                vendas.tipo_contrato_id,
+                vendas.inicio_contrato,
+                clientes.nome AS nome_cliente,
+                tipo_contrato.nome,
+                parcelas.numero_parcela,
+                vendas.valor_total,
+                vendas.porcentagem
+            FROM
+                vendas
+            JOIN
+                clientes ON vendas.cliente_id = clientes.id
+            JOIN
+                produtos ON vendas.produto_id = produtos.id
+            JOIN
+                tipo_cliente ON produtos.tipo_cliente_id = tipo_cliente.id
+            JOIN
+                tipo_contrato ON vendas.tipo_contrato_id = tipo_contrato.id
+            JOIN
+                parcelas ON vendas.parcela_id = parcelas.id 
+            WHERE
+                vendas.usuario_id = :id";       
 
-
-        $db = $this->conn->prepare($sql);
-        $db->bindParam(':id', $id);
-        $db->execute();
-
-        $relatorioComissao = $db->fetchAll(PDO::FETCH_ASSOC);
-
-        return $relatorioComissao;
-    }
+            $db = $this->conn->prepare($sql);
+            $db->bindParam(':id', $id);
+            $db->execute();
+        
+            $relatorioComissao = $db->fetchAll(PDO::FETCH_ASSOC);
+        
+            return $relatorioComissao;
+        }
 }
+
+
+            
+
+   
