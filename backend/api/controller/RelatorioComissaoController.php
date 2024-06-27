6f7b22993f19115061be2d1c78e6542aeac060cb
Katypires <katianepiresrc@gmail.com>
2024-06-27 19:47:16 -0400

    Corrigido relatório da comissão para exibir as informações.

--- backend/api/controller/RelatorioComissaoController.php
+++ backend/api/controller/RelatorioComissaoController.php
@@ -3,7 +3,6 @@ include "../database.php";
 
 class RelatorioComissaoController
 {
-
     private $conn;
 
     public function __construct()
@@ -12,39 +11,34 @@ class RelatorioComissaoController
         $this->conn = $objDb->connect();
     }
 
-    public function gerarRelatorioComissao(int $id)
+    public function gerarRelatorioComissao()
     {
-            $sql = "SELECT 
-                vendas.tipo_contrato_id,
-                vendas.inicio_contrato,
-                clientes.nome AS nome_cliente,
-                tipo_contrato.nome,
-                parcelas.numero_parcela,
-                vendas.valor_total,
-                vendas.porcentagem
-            FROM
-                vendas
-            JOIN
-                clientes ON vendas.cliente_id = clientes.id
-            JOIN
-                produtos ON vendas.produto_id = produtos.id
-            JOIN
-                tipo_cliente ON produtos.tipo_cliente_id = tipo_cliente.id
-            JOIN
-                tipo_contrato ON vendas.tipo_contrato_id = tipo_contrato.id
-            JOIN
-                parcelas ON vendas.parcela_id = parcelas.id 
-            WHERE
-                vendas.usuario_id = :id";       
+        $sql = "SELECT 
+        vendas.id AS numero_contrato,
+        vendas.inicio_contrato,
+        clientes.nome AS nome_cliente,
+        tipo_contrato.nome AS tipo_contrato,
+        vendas.numero_parcela,
+        vendas.valor_total,
+        produtos.comissaoA AS comissao_produtoA,
+        produtos.comissaoB AS comissao_produtoB
+    FROM
+        vendas
+    JOIN
+        clientes ON vendas.cliente_id = clientes.id
+    JOIN
+        produtos ON vendas.produto_id = produtos.id
+    JOIN
+        tipo_contrato ON vendas.tipo_contrato_id = tipo_contrato.id
+    WHERE
+        vendas.usuario_id = vendas.id";    
 
             $db = $this->conn->prepare($sql);
-            $db->bindParam(':id', $id);
             $db->execute();
-        
+
             $relatorioComissao = $db->fetchAll(PDO::FETCH_ASSOC);
-        
             return $relatorioComissao;
-        }
+    }
 }
 
 
