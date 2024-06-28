240a596acadada4722621489e2e090adaa629365
GustavoHZanella <139408633+GustavoHZanella@users.noreply.github.com>
2024-06-27 21:23:55 -0400

    ajustes
    Modificacoes feita na tela de ajuste

--- front_end/app/routes/ajustes/TipoCliente/page.tsx
+++ front_end/app/routes/ajustes/TipoCliente/page.tsx
@@ -1,30 +1,48 @@
 'use client'
-export default function TipoCliente() {
 
+import { createNewTipoCliente } from "@/lib/TipoClienteController";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+export default function TipoCliente() {
+    const [nomeTipoCliente, setNomeTipoCliente] = useState<string>('');
+    const [porcentagem, setPorcetagem] = useState<string>('');
+
+    const router = useRouter();
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        await createNewTipoCliente(
+            nomeTipoCliente,
+            Number(porcentagem)
+        );
+        router.push('/routes/ajustes');
+    };
 
     return (
         <div className="flex h-full justify-center items-center">
             <div className="w-full bg-white shadow-xl rounded-2xl p-8">
                 <h2 className="text-2xl font-semibold mb-8 text-center">Tipo de Cliente</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4 flex flex-col gap-5">
                         <input
                             type="text"
-                            id="tipoCliente"
-                            name="tipoCliente"
-                            // value={nomeContrato}
-                            // onChange={(event) => setNomeContrato(event.target.value)}
+                            id="nomeTipoCliente"
+                            name="nomeTipoCliente"
+                            value={nomeTipoCliente}
+                            onChange={(event) => setNomeTipoCliente(event.target.value)}
                             placeholder="Tipo de Cliente"
                             required
                             className="border-b-2 focus:border-b-2 focus:outline-none focus:border-blue-500"
                         />
                         <input
-                            type="nummber"
-                            id="porcetagem"
-                            name="porcetagem"
-                            // value={nomeContrato}
-                            // onChange={(event) => setNomeContrato(event.target.value)}
-                            placeholder="Porcetagem 99%"
+                            type="number"
+                            id="porcentagem"
+                            maxLength={3}
+                            name="porcentagem"
+                            value={porcentagem}
+                            onChange={(event) => setPorcetagem(event.target.value)}
+                            placeholder="Porcentagem 99%"
                             required
                             className="border-b-2 focus:border-b-2 focus:outline-none focus:border-blue-500"
                         />
