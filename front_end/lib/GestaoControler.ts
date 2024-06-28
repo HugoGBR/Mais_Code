e437f8e4dda2c6c44097fe9c2f1390d38370b1a1
HugoGBR <hugopn12@gmail.com>
2024-06-28 19:19:49 -0400

    Update GestaoControler.ts

--- front_end/lib/GestaoControler.ts
+++ front_end/lib/GestaoControler.ts
@@ -1,7 +1,7 @@
 import {backendURL} from "@/lib/URLS/backendURL";
 
 export async function getAllClient() {
-    const resposta = await fetch(`${backendURL()}/cliente.php?acao=getAllClient`);
+    const resposta = await fetch(`${backendURL()}/ClienteService.php?acao=getAllClient`);
     const dados = await resposta.json();
     return dados;
 }
