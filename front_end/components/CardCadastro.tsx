4f7cbce7ad0860b57557bd0acc310d0274cad189
DaniloMamedio <danilomamedio95000@gmail.com>
2024-06-28 20:31:53 -0400

    CardCadastro
    Ajustei os inputs de buscar por CPF/CNPJ e Colaborador. Ajustei tamanho, posição

--- front_end/components/CardCadastro.tsx
+++ front_end/components/CardCadastro.tsx
@@ -1,10 +1,10 @@
-import {Card} from "@/components/ui/card";
-import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import { Card } from "@/components/ui/card";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import Link from "next/link";
-import {GoGear} from "react-icons/go";
-import React, {FormEvent, useState} from "react";
-import {useRouter} from "next/navigation";
-import {createNewSell} from "@/lib/VendaController";
+import { GoGear } from "react-icons/go";
+import React, { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import { createNewSell } from "@/lib/VendaController";
 
 export default function CardCadastro() {
 
@@ -66,28 +66,28 @@ export default function CardCadastro() {
                         <h2 className="mb-5 font-bold">Dados do Contrato</h2>
                         <div className="md:grid md:grid-cols-2 gap-5 mb-5">
                             <div className="md:grid md:grid-cols-2 gap-5 mt-5">
-                                <input className="border-b-2 focus:outline-none focus:border-blue-500"
-                                       placeholder="CPF/CNPJ do Cliente"
-                                       type="text"/>
+                                <input className="border-b-2 h-6 mt-auto focus:outline-none focus:border-blue-500"
+                                    placeholder="CPF/CNPJ do Cliente"
+                                    type="text" />
                                 <button type="submit"
-                                        className=" mt-5 w-28 px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                                    className="w-28 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
                                     BUSCAR
                                 </button>
                             </div>
 
-                            <input className="border-b-2 focus:outline-none focus:border-blue-500 mb-5 invisible"
-                                   placeholder="Colaborador"
-                                   type="text"/>
+                            <input className="border-b-2 mt-auto focus:outline-none focus:border-blue-500 invisible"
+                                placeholder="Colaborador"
+                                type="text" />
                             <div className="flex flex-col mb-5">
                                 <label className="text-sm" htmlFor="teste">Data Inicio</label>
                                 <input className="border-b-2 focus:outline-none focus:border-blue-500"
-                                       placeholder="Data de inicio" type="date"
-                                       onChange={(event) => setDataInicio(event.target.value)}/>
+                                    placeholder="Data de inicio" type="date"
+                                    onChange={(event) => setDataInicio(event.target.value)} />
                             </div>
                             <div className="flex flex-col mb-5">
                                 <label className="text-sm" htmlFor="teste">Data Termino</label>
                                 <input className="border-b-2 focus:outline-none focus:border-blue-500" type="date"
-                                       onChange={(event) => setDataFim(event.target.value)}/>
+                                    onChange={(event) => setDataFim(event.target.value)} />
                             </div>
                         </div>
 
@@ -96,7 +96,7 @@ export default function CardCadastro() {
                                 <label className="col-span-2 text-sm" htmlFor="teste">Modelo do Contratos</label>
                                 <Select>
                                     <SelectTrigger className="h-8 mt-1 mb-4 rounded-lg w-36">
-                                        <SelectValue placeholder="Tipo Contrato"/>
+                                        <SelectValue placeholder="Tipo Contrato" />
                                     </SelectTrigger>
                                     <SelectContent>
                                         <SelectItem value="teste">Pontual</SelectItem>
@@ -104,11 +104,10 @@ export default function CardCadastro() {
                                     </SelectContent>
                                 </Select>
 
-
                                 <label className="col-span-2 text-sm" htmlFor="teste">Produto</label>
                                 <Select>
                                     <SelectTrigger className="h-8 mt-1 rounded-lg w-36">
-                                        <SelectValue placeholder="Produto"/>
+                                        <SelectValue placeholder="Produto" />
                                     </SelectTrigger>
                                     <SelectContent>
                                         <SelectItem value="teste">Python</SelectItem>
@@ -120,22 +119,22 @@ export default function CardCadastro() {
                                 <label className="text-sm mb-2" htmlFor="teste">Horas Trabalhadas</label>
                                 <input className="border-b-2 focus:outline-none focus:border-blue-500"
                                     // onChange={(event) => setHoraTrabalhada(event.target.value)}
-                                       placeholder="Horas" type="number"/>
+                                    placeholder="Horas" type="number" />
                             </div>
                         </div>
 
                         <h2 className="font-bold">Dados do Contato</h2>
                         <div className="grid grid-cols-2 gap-5 mt-5">
                             <input className="border-b-2 focus:outline-none focus:border-blue-500"
-                                   placeholder="Nome"
-                                   onChange={(event) => setNomeContato(event.target.value)}
-                                   type="text"/>
+                                placeholder="Nome"
+                                onChange={(event) => setNomeContato(event.target.value)}
+                                type="text" />
                             <input className="border-b-2 focus:outline-none focus:border-blue-500"
-                                   placeholder="(99) 99999-9999"
-                                   onChange={(event) => setTelefoneContato(event.target.value)} type="tel"/>
+                                placeholder="(99) 99999-9999"
+                                onChange={(event) => setTelefoneContato(event.target.value)} type="tel" />
                             <input className="border-b-2 focus:outline-none focus:border-blue-500"
-                                   placeholder="Email" onChange={(event) => setEmailContato(event.target.value)}
-                                   type="email"/>
+                                placeholder="Email" onChange={(event) => setEmailContato(event.target.value)}
+                                type="email" />
                         </div>
                     </form>
                 </Card>
@@ -150,15 +149,15 @@ export default function CardCadastro() {
                         <div className="flex mb-4 ">
                             <label className="mr-4" htmlFor="teste">Valor da Entrada</label>
                             <input className="border-b-2 w-28 focus:outline-none focus:border-blue-500"
-                                   placeholder="R$ 0000,00" type="text"
-                                   onChange={(event) => setValorEntrada(event.target.value)}/>
+                                placeholder="R$ 0000,00" type="text"
+                                onChange={(event) => setValorEntrada(event.target.value)} />
                         </div>
 
                         <div className="mb-5">
                             <label className="text-sm" htmlFor="Nn">Status Cliente</label>
                             <Select>
                                 <SelectTrigger className="h-8 mt-2 rounded-lg w-36">
-                                    <SelectValue placeholder="Tipo"/>
+                                    <SelectValue placeholder="Tipo" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectItem value="teste">Novo</SelectItem>
@@ -172,25 +171,25 @@ export default function CardCadastro() {
                             <div className="flex gap-3">
                                 <div className="flex items-center">
                                     <input id="pagamento-opcao-1" type="radio" name="forma-pagamento"
-                                           value="À vista"
-                                           className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
-                                           aria-labelledby="pagamento-opcao-1"
-                                           aria-describedby="pagamento-opcao-1"
-                                           onClick={() => setMostrarParcelas(false)}/>
+                                        value="À vista"
+                                        className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+                                        aria-labelledby="pagamento-opcao-1"
+                                        aria-describedby="pagamento-opcao-1"
+                                        onClick={() => setMostrarParcelas(false)} />
                                     <label htmlFor="pagamento-opcao-1"
-                                           className="block ml-2 text-sm font-medium text-gray-900">
+                                        className="block ml-2 text-sm font-medium text-gray-900">
                                         À vista
                                     </label>
                                 </div>
                                 <div className="flex items-center">
                                     <input id="pagamento-opcao-2" type="radio" name="forma-pagamento"
-                                           value="Parcelado"
-                                           className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
-                                           aria-labelledby="pagamento-opcao-2"
-                                           aria-describedby="pagamento-opcao-2"
-                                           onClick={() => setMostrarParcelas(true)}/>
+                                        value="Parcelado"
+                                        className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+                                        aria-labelledby="pagamento-opcao-2"
+                                        aria-describedby="pagamento-opcao-2"
+                                        onClick={() => setMostrarParcelas(true)} />
                                     <label htmlFor="pagamento-opcao-2"
-                                           className="block ml-2 text-sm font-medium text-gray-900">
+                                        className="block ml-2 text-sm font-medium text-gray-900">
                                         Parcelado
                                     </label>
                                 </div>
@@ -204,7 +203,7 @@ export default function CardCadastro() {
                                         type="text"
                                     />
                                     <Link href="">
-                                        <GoGear className="w-8 h-8"/>
+                                        <GoGear className="w-8 h-8" />
                                     </Link>
                                 </div>
                             )}
@@ -216,7 +215,7 @@ export default function CardCadastro() {
                         </div>
                         <div className="mt-5 text-center">
                             <button type="submit"
-                                    className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                                className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
                                 CADASTRAR
                             </button>
                         </div>
