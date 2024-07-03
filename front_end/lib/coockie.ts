3eadb5de514f8f3de8b9c712921a60719c11b9f8
Victor Vicente <victorvpenteadog@gmail.com>
2024-07-03 18:55:55 -0400

    bem vindo feito com nome do usuario

--- front_end/lib/coockie.ts
+++ front_end/lib/coockie.ts
@@ -1,18 +1,17 @@
 "use server"
 import {cookies} from "next/headers";
 
-export async function criarCookie(cookieName: string, values:string) {
-
+export async function criarCookie(cookieName: string, values: string) {
     cookies().set({
         name: cookieName,
         value: values,
         httpOnly: true,
-    })
+    });
 }
 
-export async function getCookie() {
+export async function getCookie(cookieName: string) {
     const allCookies = cookies();
-    const userCookie = allCookies.get('CookiCriado');
+    const userCookie = allCookies.get(cookieName);
 
     if (userCookie) {
         return userCookie.value;
