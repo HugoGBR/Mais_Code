f85d1e854942796c4e95c1ad6d9b632d7ee60d52
Victor Vicente <victorvpenteadog@gmail.com>
2024-07-09 20:41:57 -0400

    tamanho da barra de pesquisa de ajuste maior

--- front_end/app/routes/ajustes/TableProdutos/TableProduto.tsx
+++ front_end/app/routes/ajustes/TableProdutos/TableProduto.tsx
@@ -89,7 +89,7 @@ export function TableProduto<TData, TValue>({
             onChange={(event) =>
               table.getColumn("nome")?.setFilterValue(event.target.value)
             }
-            className="max-w-36 border border-gray-300 rounded-md p-2 mb-2 focus:outline-none focus:border-blue-500"
+            className="max-w-full border border-gray-300 rounded-md p-2 mb-2 focus:outline-none focus:border-blue-500"
           />
           <div className="rounded-lg border h-96">
             <Table>
