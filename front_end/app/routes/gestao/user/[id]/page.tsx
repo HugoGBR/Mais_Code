1541fcdb3f01251558ab80c24fe6307072fa917f
GustavoHZanella <139408633+GustavoHZanella@users.noreply.github.com>
2024-10-01 20:36:30 -0400

    Feito Ajuste no mostrar senha em Getão
    Realizado o botão de mostrar senha na tela de gestão de usuário

--- front_end/app/routes/gestao/user/[id]/page.tsx
+++ front_end/app/routes/gestao/user/[id]/page.tsx
@@ -17,6 +17,7 @@ export default function App({ params }: { params: { id: number } }) {
     const [dadosUsuario, setdadosUsuario] = useState({ nome: "", cargo_id: 0, senha: "", email: "", status_usuario: 1 });
     const router = useRouter();
     const [inputsHabilitados, setInputHabilitados] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const HabilitarEventos = () => {
         setInputHabilitados(true);
@@ -41,10 +42,10 @@ export default function App({ params }: { params: { id: number } }) {
             setdadosUsuario(usuario);
             setValue('cargo', usuario.cargo_id.toString());
         };
-    
+
         setdados();
     }, [params.id, setValue]);
-    
+
 
     const handleEditSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -63,9 +64,9 @@ export default function App({ params }: { params: { id: number } }) {
 
     const toggleUserStatus = async () => {
         try {
-            const novoStatus = dadosUsuario.status_usuario === 1 ? 0 : 1; 
+            const novoStatus = dadosUsuario.status_usuario === 1 ? 0 : 1;
             setdadosUsuario((prev) => ({ ...prev, status_usuario: novoStatus }));
-            
+
         } catch (error) {
             console.error("Erro ao alterar o status do usuário: ", error);
         }
@@ -92,7 +93,7 @@ export default function App({ params }: { params: { id: number } }) {
                                     onChange={(e) => setdadosUsuario({ ...dadosUsuario, nome: e.target.value })}
                                     className="border-b-2 focus:border-b-2 focus:outline-none focus:border-blue-500"
                                     id="nome" placeholder="Nome"
-                                    disabled={!inputsHabilitados } 
+                                    disabled={!inputsHabilitados}
                                 />
                             </div>
                             <div className="flex flex-col space-y-1.5">
@@ -102,23 +103,58 @@ export default function App({ params }: { params: { id: number } }) {
                                     onChange={(e) => setdadosUsuario({ ...dadosUsuario, email: e.target.value })}
                                     className="border-b-2 focus:border-b-2 focus:outline-none focus:border-blue-500"
                                     id="email" placeholder="Email"
-                                    disabled={!inputsHabilitados } 
+                                    disabled={!inputsHabilitados}
                                 />
                             </div>
-                            <div className="flex flex-col space-y-1.5">
-                                <input
-                                    type="password"
-                                    value={dadosUsuario.senha}
-                                    onChange={(e) => setdadosUsuario({ ...dadosUsuario, senha: e.target.value })}
-                                    className="border-b-2 focus:border-b-2 focus:outline-none focus:border-blue-500"
-                                    id="senha" placeholder="Senha"
-                                    disabled={!inputsHabilitados } 
-                                />
+                            <div className="max-w-sm">
+                                <div className="relative">
+                                    <input
+                                        id="hs-toggle-password"
+                                        type={showPassword ? "text" : "password"}
+                                        value={dadosUsuario.senha}
+                                        onChange={(e) => setdadosUsuario({ ...dadosUsuario, senha: e.target.value })}
+                                        className="border-b-2 ps-2 focus:border-b-2 focus:outline-none focus:border-blue-500 h-10 w-full pr-10"  // pr-10 para evitar que o botão sobreponha o texto
+                                        placeholder="Senha"
+                                        disabled={!inputsHabilitados}
+                                    />
+
+                                    <button
+                                        type="button"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        className="absolute inset-y-0 right-0 flex items-center px-3 cursor-pointer text-gray-400"
+                                    >
+                                        <svg
+                                            className="shrink-0 size-3.5"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        >
+                                            {showPassword ? (
+                                                <>
+                                                    <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+                                                    <circle cx="12" cy="12" r="3"></circle>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
+                                                    <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
+                                                    <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
+                                                    <line x1="2" x2="22" y1="2" y2="22"></line>
+                                                </>
+                                            )}
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Select
                                     value={dadosUsuario.cargo_id.toString()}
-                                    disabled={!inputsHabilitados || dadosUsuario.status_usuario === 0} 
+                                    disabled={!inputsHabilitados || dadosUsuario.status_usuario === 0}
                                     onValueChange={(value) => setdadosUsuario({ ...dadosUsuario, cargo_id: parseInt(value) })}>
                                     <SelectTrigger className="w-[220px]">
                                         <SelectValue placeholder="Cargos..." />
@@ -140,7 +176,7 @@ export default function App({ params }: { params: { id: number } }) {
                                 type="button"
                                 onClick={toggleUserStatus}
                                 className={`w-full ${dadosUsuario.status_usuario === 1 ? 'bg-red-700' : 'bg-green-700'} hover:bg-opacity-75 text-white font-bold py-2 px-4 rounded`}
-                                disabled={!inputsHabilitados} 
+                                disabled={!inputsHabilitados}
                             >
                                 {dadosUsuario.status_usuario === 1 ? "Inativar Usuário" : "Ativar Usuário"}
                             </button>
