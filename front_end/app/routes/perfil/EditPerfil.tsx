080dd163bb419cb57b9a74462bfd713217d7af7e
DaniloMamedio <danilomamedio95000@gmail.com>
2024-10-01 21:04:29 -0400

    Victor por aq tirei botao de editperfil que deu um trabalho da pega pra fazer trabalho desnecessario odeios todos faz o L

--- front_end/app/routes/perfil/EditPerfil.tsx
+++ front_end/app/routes/perfil/EditPerfil.tsx
@@ -147,10 +147,7 @@ export default function Perfil() {
                             </div>
                         </div>
                         <div className='flex justify-center'>
-                            <button type='button' onClick={handleButtonClick}
-                                className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                {inputsHabilitados ? "Alterar" : "Editar"}
-                            </button>
+                        
                         </div>
                     </Card>
                 </div>
