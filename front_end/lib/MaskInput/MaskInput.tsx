f59f868ae03fcd72acb8c7177894db8b794e0bb3
JaoNaves <joaoluiznaves@hotmail.com>
2024-10-30 19:15:38 -0400

    limpeza nos comentarios da tela de mascaras

--- front_end/lib/MaskInput/MaskInput.tsx
+++ front_end/lib/MaskInput/MaskInput.tsx
@@ -19,41 +19,40 @@ export const insertMaskTelefone = (telefone: string) => {
     return maskedTelefone;
 };
 export const insertMaskValorMonetarioSemVirgula = (valor: string) => {
-    // Remove caracteres não numéricos
+
     let numericValor = valor.replace(/\D/g, '').slice(0, 15);
 
-    // Se a entrada estiver vazia, retorne uma string vazia
+
     if (numericValor === '') {
         return '';
     }
 
-    // Garante que o valor tenha pelo menos dois dígitos para os centavos
+
     let valorComCentavos = numericValor.padStart(3, '0');
     
-    // Divide o valor em reais e centavos
     const reais = valorComCentavos.slice(0, -2);
     const centavos = valorComCentavos.slice(-2);
 
-    // Formata o valor com separador de milhar
+
     const formattedReais = Number(reais).toLocaleString('pt-BR');
 
-    // Combina os reais com os centavos
+
     return `${formattedReais}.${centavos}`;
 };
 
 export const removerMascaraValorMonetario = (valor: string) => {
-    // Remove caracteres não numéricos e o ponto decimal
+
     let numericValor = valor.replace(/[^\d]/g, '');
 
-    // Se a entrada estiver vazia, retorne 0
+
     if (numericValor === '') {
         return 0;
     }
 
-    // Converte o valor numérico em string para um número
+
     const valorNumerico = parseFloat(numericValor.slice(0, -2) + '.' + numericValor.slice(-2));
 
-    return valorNumerico || 0; // Retorna 0 se não for um número válido
+    return valorNumerico || 0;
 };
 
 
